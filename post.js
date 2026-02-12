8245eebb92d5c981b2ecd90e44de5abdae664db9
Rasel0406 <durjoychowdhury0406@gmail.com>
2026-02-13 03:11:25 +0600

    post card add

---
+++ post.js
@@ -0,0 +1,23 @@
+const postData = ()=>{
+    const url = "https://jsonplaceholder.typicode.com/posts"
+    fetch(url)
+    .then((res)=> res.json())
+    .then((data) => displaypost(data))
+};
+ const displaypost = (posts)=>{
+    const postContainer = document.getElementById("post_container");
+    postContainer.innerHTML = "";
+    posts.forEach((post)=>{
+        console.log(post);
+        const div = document.createElement("div")
+        div.innerHTML = `
+         <div class="post_card">
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+                    </div>
+        `
+        postContainer.appendChild(div);
+   
+    })
+
+ }
